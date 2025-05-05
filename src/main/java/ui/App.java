d83c61cb07b4d847b703a28743205dd5354a008e
Sebastian <sebastian@familienholdt.dk>
2025-05-05 11:38:41 +0200

    Add createProject to UI

--- src/main/java/ui/App.java
+++ src/main/java/ui/App.java
@@ -1,21 +1,51 @@
 package ui;
 
+import app.SystemApp;
+import app.SystemAppException;
+
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) {
+
+    private final SystemApp pma = new SystemApp();
+
+    private void createProject(Scanner arguments) {
+        if (!arguments.hasNext()) {
+            System.out.println("Usage: createProject <name>");
+            return;
+        }
+        String name = arguments.next();
+        try {
+            pma.createProject(name);
+        } catch (SystemAppException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void launch() {
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.print("# ");
             String line = scanner.nextLine();
             Scanner lineScanner = new Scanner(line);
-            String command = lineScanner.next();
+            String command = "";
+            if (lineScanner.hasNext()) {
+                command = lineScanner.next();
+            }
             switch (command.toLowerCase()) {
+                case "createproject":
+                    createProject(lineScanner);
+                    break;
                 case "exit":
                     return;
                 default:
                     System.out.println("Unknown command");
             }
         }
+    }
+
+    public static void main(String[] args) {
+        App ui = new App();
+        ui.launch();
     }
 }
