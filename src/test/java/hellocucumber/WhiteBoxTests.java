e83928362ec8df57db6e55bfa0cb469a2ed7d358
karl3009 <s235481@dtu.dk>
2025-05-10 13:47:07 +0200

    whitebox

---
+++ src/test/java/hellocucumber/WhiteBoxTests.java
@@ -0,0 +1,45 @@
+package hellocucumber;
+
+import app.SystemApp;
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
+
+
+public class WhiteBoxTests {
+    private SystemApp systemApp = new SystemApp();
+    private String errorMessage;
+
+    @Test
+    void testRegisterTimeA() {
+        try {
+            systemApp.createFirmActivity("a");
+        } catch (Exception e){
+            errorMessage = e.getMessage();
+        }
+
+        try {
+            systemApp.registerTimeFirmActivity("huba", "a", 25, 0, 1, 1, 2025);
+        } catch (Exception e){
+            errorMessage = e.getMessage();
+        }
+        assertEquals("Cannot work more than 24 hours a day", errorMessage);
+    }
+//    @Test
+//    void testRegisterTimeB() {
+//        try {
+//            systemApp.createFirmActivity("a");
+//        } catch (Exception e){
+//            errorMessage = e.getMessage();
+//        }
+//
+//        try {
+//            systemApp.registerTimeFirmActivity("huba", "a", 4, 0, 1, 1, 2025);
+//        } catch (Exception e){
+//            errorMessage = e.getMessage();
+//        }
+//        assertEquals(4,systemApp.checkRegisteredFirmActivity("huba","a",1,1,2025));
+//    }
+
+
+
+}
