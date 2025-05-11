3f7dc1d547825416f9bda20c1135c24fdac350b6
Dejan <deja0079@outlook.dk>
2025-05-11 12:18:09 +0200

    Suppress warnings in UnitSteps.java

--- src/test/java/hellocucumber/UnitSteps.java
+++ src/test/java/hellocucumber/UnitSteps.java
@@ -1,22 +1,23 @@
 package hellocucumber;
 
 import app.*;
-import java.util.*;
 
 import io.cucumber.java.en.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class UnitSteps {
     private boolean boolResult;
-    private Employee testEmployee = new Employee("test");
+    private final Employee testEmployee = new Employee("test");
     @When("comparing if an employee object equals null")
     public void comparingIfAnEmployeeObjectEqualsNull() {
+        //noinspection ConstantValue
         boolResult = testEmployee.equals(null);
     }
 
     @When("comparing if an employee object equals an activity object")
     public void comparingIfAnEmployeeObjectEqualsAnActivityObject() {
         try {
+            //noinspection EqualsBetweenInconvertibleTypes
             boolResult = testEmployee.equals(new Activity("test"));
         } catch (Exception e) {
             fail(e.getMessage());
